ba29fc636b16cd8010ab18ecbc95eb9329fae5fe
Mason Currie <masoncu13@ymail.com>
2023-11-02 15:45:45 -0500

    complete
    added feature to add and delete notes

--- react-app/src/components/Note.jsx
+++ react-app/src/components/Note.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Note(props) {
+
+  function handleClicik(){
+    props.onDelete(props.id);
+  }
+  
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button>DELETE</button>
+      <button onClick={handleClicik}>DELETE</button>
     </div>
   );
 }
